fdf4dc850eafdbf38c1d429cd97904bf365c482f
robotchaoX <948037851@qq.com>
2020-01-09 00:08:02 +0800

    python basic
    print comment variable operator type input format output

--- 01_基础/01_hello.py
+++ 01_基础/01_hello.py
@@ -1,2 +1,7 @@
+# !/usr/bin/python3
+# coding=utf-8
+
 print("hello")
-print("hh")
+print("python3")
+print("你好")
+# 后缀名不是 .py 也可以
--- 01_基础/09_格式化输出.py
+++ 01_基础/09_格式化输出.py
@@ -1,13 +1,30 @@
-name="小明"
-print("我的名字叫%s，请多关照" %name)
+# 定义字符串变量 name，输出 我的名字叫 小明，请多多关照！
+name = "小明"
+print("我的名字叫 %s" % name)  # % 一个变量
+age = 18
+print("name: %s , age: %d" % (name, age))  # %( , ) 两个变量
 
-student_num = 123
-print("我的学号是%6d" %student_num )
+# 定义整数变量 student_no，输出 我的学号是 000001
+student_cnt = 3
+print("学生数量: %06d" % student_cnt)  # 位数不足补0
+student_no = 123456789
+print("我的学号: %06d" % student_no)  # 超了不影响
 
-price =9.0
-weight = 5.0
+# 定义小数 price、weight、money，
+# 输出 苹果单价 9.00 元／斤，购买了 5.00 斤，需要支付 45.00 元
+price = 8.5
+weight = 7.5
 money = price * weight
-print("单价：%.2f 重量：%.2f 金额：%.2f" %(price, weight, money))
+print("苹果单价 %.2f 元／斤，购买了 %.3f 斤，需要支付 %.4f 元" % (price, weight, money))  # %.2f小数点后2位
 
-scale = 0.25
-print("比例：%.2f%%" %(scale*100))
+# 定义一个小数 scale，输出 数据比例是 10.00%
+scale = 0.8
+print("数据比例是 %.2f%%" % (scale * 100))
+# print("数据比例是 %.2f%%" % scale * 100)  # 重复100次 # 等价 # print(("数据比例是 %.2f%%" % scale) * 100)
+
+import keyword
+
+print(keyword.kwlist)
+"""
+['False', 'None', 'True', 'and', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+"""
