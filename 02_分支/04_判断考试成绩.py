7567127d37d2af0d71810e23037d2f678a9a63a4
robotchaoX <948037851@qq.com>
2020-01-09 12:14:32 +0800

    if branch
    if :   elif:   else:
    and, or, not
    import random

--- 02_分支/04_判断考试成绩.py
+++ 02_分支/04_判断考试成绩.py
@@ -1,6 +1,9 @@
+# 定义两个整数变量 python_score、c_score，编写代码判断成绩
 py_score = 50
-c_score = 30
-if py_score >= 60 or c_score >= 60:
+c_score = 80
+
+# 要求只要有一门成绩 > 60 分就算合格
+if py_score >= 60 or c_score >= 60:  # or
     print("考试通过")
 else:
     print("考试失败")
--- 02_分支/08_石头剪刀布.py
+++ 02_分支/08_石头剪刀布.py
@@ -1,16 +1,31 @@
+# 导入随机工具包
+# 注意：在导入工具包的时候，应该将导入的语句，放在文件的顶部
+# 因为，这样可以方便下方的代码，在任何需要的时候，使用工具包中的工具
 import random
 
-player = int(input("请输入 石头(1) /剪刀(2) /布(3):"))
-computer = random.randint(1, 3)
+# 从控制台输入要出的拳 —— 石头（1）／剪刀（2）／布（3）
+player = int(input("请输入您要出的拳 石头（1）／剪刀（2）／布（3）："))
 
-print("玩家出的是%d 电脑出的是%d" % (player, computer))
-# 比较胜负
-# 1玩家胜
-if (player == 1 and computer == 2) \
-        or (player == 2 and computer == 3) \
-        or (player == 3 and computer == 1):
+# 电脑 随机 出拳 —— 先假定电脑只会出石头，完成整体代码功能
+computer = random.randint(1, 3)  # python区间为 双闭区间 [1,3]
+
+print("玩家选择的拳头是 %d - vs - 电脑出的拳是 %d" % (player, computer))
+
+# 比较胜负,以玩家为中心分情况
+# 1	石头 胜 剪刀
+# 2	剪刀 胜 布
+# 3	布   胜 石头
+# if ( ()
+#        or ()
+#        or () ):
+# 玩家胜   # ()括号内代码可任意换行     # \ 表示续行
+if ((player == 1 and computer == 2)
+        or (player == 2 and computer == 3)
+        or (player == 3 and computer == 1)):  # 1玩家胜
     print("玩家胜")
+# 平局
 elif player == computer:  # 2平局
     print("平局")
+# 其他的情况就是电脑获胜
 else:  # 3电脑胜(其他)
     print("电脑胜")
